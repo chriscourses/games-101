78b910cf8e632da2f633cd96b6a760cef9e8dabd
christopher4lis <christopher4lis@gmail.com>
2022-03-31 13:14:30 -0400

    Create a Player

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'blue')
+player.draw()
