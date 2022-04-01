865d4c86aeb574c6469a5e7c7e2bce5322d0b115
christopher4lis <christopher4lis@gmail.com>
2022-03-31 22:11:47 -0400

    Colorize Game

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 const projectiles = []
 const enemies = []
 
@@ -88,7 +88,7 @@ function spawnEnemies() {
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
     }
 
-    const color = 'green'
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
 
@@ -104,7 +104,8 @@ function spawnEnemies() {
 let animationId
 function animate() {
   animationId = requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
   projectiles.forEach((projectile, index) => {
     projectile.update()
@@ -153,11 +154,11 @@ addEventListener('click', (event) => {
     event.clientX - canvas.width / 2
   )
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle)
+    x: Math.cos(angle) * 5,
+    y: Math.sin(angle) * 5
   }
   projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
   )
 })
 
