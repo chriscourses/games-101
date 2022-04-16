a66c28df6d70edf9db8efdf93e58756bb21744f2
christopher4lis <christopher4lis@gmail.com>
2022-04-16 16:13:21 -0400

    More Accurate Object Removal

--- index.js
+++ index.js
@@ -141,15 +141,22 @@ function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
+
   player.draw()
-  particles.forEach((particle, index) => {
+
+  for (let index = particles.length - 1; index >= 0; index--) {
+    const particle = particles[index]
+
     if (particle.alpha <= 0) {
       particles.splice(index, 1)
     } else {
       particle.update()
     }
-  })
-  projectiles.forEach((projectile, index) => {
+  }
+
+  for (let index = projectiles.length - 1; index >= 0; index--) {
+    const projectile = projectiles[index]
+
     projectile.update()
 
     // remove from edges of screen
@@ -159,13 +166,13 @@ function animate() {
       projectile.y + projectile.radius < 0 ||
       projectile.y - projectile.radius > canvas.height
     ) {
-      setTimeout(() => {
-        projectiles.splice(index, 1)
-      }, 0)
+      projectiles.splice(index, 1)
     }
-  })
+  }
 
-  enemies.forEach((enemy, index) => {
+  for (let index = enemies.length - 1; index >= 0; index--) {
+    const enemy = enemies[index]
+
     enemy.update()
 
     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
@@ -175,7 +182,13 @@ function animate() {
       cancelAnimationFrame(animationId)
     }
 
-    projectiles.forEach((projectile, projectilesIndex) => {
+    for (
+      let projectilesIndex = projectiles.length - 1;
+      projectilesIndex >= 0;
+      projectilesIndex--
+    ) {
+      const projectile = projectiles[projectilesIndex]
+
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
       // when projectiles touch enemy
@@ -202,22 +215,18 @@ function animate() {
           gsap.to(enemy, {
             radius: enemy.radius - 10
           })
-          setTimeout(() => {
-            projectiles.splice(projectilesIndex, 1)
-          }, 0)
+          projectiles.splice(projectilesIndex, 1)
         } else {
           // remove enemy if they are too small
           score += 150
           scoreEl.innerHTML = score
 
-          setTimeout(() => {
-            enemies.splice(index, 1)
-            projectiles.splice(projectilesIndex, 1)
-          }, 0)
+          enemies.splice(index, 1)
+          projectiles.splice(projectilesIndex, 1)
         }
       }
-    })
-  })
+    }
+  }
 }
 
 addEventListener('click', (event) => {
