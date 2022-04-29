b218e90713351f514cb18cce3d8ecbbade400852
Christopher Lis <christopher4lis@gmail.com>
2022-04-28 22:13:27 -0400

    Homing Enemies

--- index.js
+++ index.js
@@ -90,6 +90,11 @@ class Enemy {
     this.radius = radius
     this.color = color
     this.velocity = velocity
+    this.type = 'Linear'
+
+    if (Math.random() < 0.5) {
+      this.type = 'Homing'
+    }
   }
 
   draw() {
@@ -101,6 +106,13 @@ class Enemy {
 
   update() {
     this.draw()
+
+    if (this.type === 'Homing') {
+      const angle = Math.atan2(player.y - this.y, player.x - this.x)
+      this.velocity.x = Math.cos(angle)
+      this.velocity.y = Math.sin(angle)
+    }
+
     this.x = this.x + this.velocity.x
     this.y = this.y + this.velocity.y
   }
